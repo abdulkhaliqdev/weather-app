3e647b7533002cfbccb03edcaaa24f0e7cebd037
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-08-07 00:49:02 +0500

    Add linters and fix all linter errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
 import './style.css';
 import Header from './header';
 
-const main = document.createElement("div");
-main.setAttribute("id", "main");
+const main = document.createElement('div');
+main.setAttribute('id', 'main');
 async function getdata(location) {
-  if ((typeof(document.getElementById('weather')) !== 'undefined') && (document.getElementById('weather') !== null)) {
+  if ((typeof (document.getElementById('weather')) !== 'undefined') && (document.getElementById('weather') !== null)) {
     document.getElementById('weather').remove();
   }
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f31d1d7e61a6d0091456d075c434ce78`;
-  const respon = await fetch(url, { origin: "cors" });
-  const weather = document.createElement("div");
-  weather.setAttribute("id", "weather");
-  weather.classList.add("weather");
-  if (respon.status === 404){
-    weather.innerHTML = `<h2 class='card'>Location Not Found! Enter Valid Location</h2>`;
-    document.body.setAttribute("class", "unknown_location");
-  }else{
-    const data = await respon.json();    
+  const respon = await fetch(url, { origin: 'cors' });
+  const weather = document.createElement('div');
+  weather.setAttribute('id', 'weather');
+  weather.classList.add('weather');
+  if (respon.status === 404) {
+    weather.innerHTML = '<h2 class=\'card\'>Location Not Found! Enter Valid Location</h2>';
+    document.body.setAttribute('class', 'unknown_location');
+  } else {
+    const data = await respon.json();
     const temp = KtoC(data.main.temp);
     weather.innerHTML = `
     <div class='card'>
@@ -25,14 +27,14 @@ async function getdata(location) {
     <small>${data.weather[0].main}</small>
     </div>
     `;
-    if(data.weather[0].main === 'Clear'){
-      document.body.setAttribute("class", "Sunny");
-    }else if(data.weather[0].main === 'Clouds'){
-      document.body.setAttribute("class", "Clouds");
-    }else if(data.weather[0].main === 'Snow'){
-      document.body.setAttribute("class", "Snow");
-    }else if(data.weather[0].main === 'Rain'){
-      document.body.setAttribute("class", "Rain");
+    if (data.weather[0].main === 'Clear') {
+      document.body.setAttribute('class', 'Sunny');
+    } else if (data.weather[0].main === 'Clouds') {
+      document.body.setAttribute('class', 'Clouds');
+    } else if (data.weather[0].main === 'Snow') {
+      document.body.setAttribute('class', 'Snow');
+    } else if (data.weather[0].main === 'Rain') {
+      document.body.setAttribute('class', 'Rain');
     }
   }
 
@@ -44,34 +46,34 @@ function KtoC(temp) {
 }
 
 function DisplayWeather(data) {
-  const form = document.createElement("div");
-  form.setAttribute("id", "form");
-  const input = document.createElement("input");
-  input.setAttribute("type", "search");
-  input.setAttribute("placeholder", "search");
-  input.setAttribute("id", "search");
-  const submit = document.createElement("input");
-  submit.setAttribute("id", "submit");
-  submit.setAttribute("type", "button");
-  submit.setAttribute("value", "submit");
+  const form = document.createElement('div');
+  form.setAttribute('id', 'form');
+  const input = document.createElement('input');
+  input.setAttribute('type', 'search');
+  input.setAttribute('placeholder', 'search');
+  input.setAttribute('id', 'search');
+  const submit = document.createElement('input');
+  submit.setAttribute('id', 'submit');
+  submit.setAttribute('type', 'button');
+  submit.setAttribute('value', 'submit');
 
   form.appendChild(input);
   form.appendChild(submit);
-  form.addEventListener("click", (e) => {
+  form.addEventListener('click', (e) => {
     e.preventDefault();
     if (
-      (e.target.tagName === "INPUT" &&
-        e.target.type !== "search" &&
-        document.getElementById("search").value !== "") ||
-      e.Keycode == 13
+      (e.target.tagName === 'INPUT'
+        && e.target.type !== 'search'
+        && document.getElementById('search').value !== '')
+      || e.Keycode === 13
     ) {
-      const search = document.getElementById("search");
+      const search = document.getElementById('search');
       getdata(search.value);
     }
   });
 
   main.appendChild(document.body.appendChild(form));
-  getdata("london");
+  getdata('london');
 }
 
 document.body.appendChild(Header());
